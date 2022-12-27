VoltageOS/packages_apps_Powerhub

Powerhub:incall vibration options [3/3]
*allow setting vibration when call is connected
*allow setting vibration when call is disconnected
*allow setting vibration for call waiting
*this works with google and aosp dialer :)

Change-Id: Id1a431124174571f8d0091825af2418971cfe35d
Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/power/hub/fragments/NotificationSettings.java
@@ -31,6 +31,7 @@ import com.power.hub.preferences.SystemSettingListPreference;
 import com.power.hub.preferences.SystemSettingSwitchPreference;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import com.android.internal.util.voltage.VoltageUtils;
+import com.power.hub.preferences.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +39,7 @@ import java.util.List;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class NotificationSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener{
 
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -46,6 +48,11 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
+		
+		PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+                prefScreen.removePreference(incallVibCategory);
+        }
         
     }
 

Creative-OS/android_packages_apps_Creativehub

src/com/creative/hub/fragments/NotificationSettings.java
@@ -31,6 +31,7 @@ import com.creative.hub.preferences.SystemSettingListPreference;
 import com.creative.hub.preferences.SystemSettingSwitchPreference;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import com.android.internal.util.creative.CreativeUtils;
+import com.creative.hub.preferences.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +39,7 @@ import java.util.List;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class NotificationSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener{
 
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -46,6 +48,11 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
+		
+		PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+                prefScreen.removePreference(incallVibCategory);
+        }
         
     }
 