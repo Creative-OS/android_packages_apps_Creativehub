366bec0693b830b1b2c4faa5955c856eeabc5878
Alvin Francis <nivlafx@gmail.com>
2022-12-26 21:37:07 -0400

    CreativeHub

---
+++ src/com/creative/hub/fragments/NotificationSettings.java
@@ -0,0 +1,87 @@
+package com.creative.hub.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.content.ContentResolver;
+import com.android.settings.R;
+import android.content.res.Resources;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import android.provider.Settings;
+import androidx.preference.ListPreference;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+import android.provider.SearchIndexableResource;
+import com.creative.hub.preferences.SystemSettingMasterSwitchPreference;
+import com.creative.hub.preferences.SystemSettingSeekBarPreference;
+import com.creative.hub.preferences.CustomSeekBarPreference;
+import com.creative.hub.preferences.SystemSettingListPreference;
+import com.creative.hub.preferences.SystemSettingSwitchPreference;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.android.internal.util.creative.CreativeUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+public class NotificationSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener{
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.creativehub_notifications);
+        ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources res = getResources();
+        
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CREATIVE;
+    }
+	
+	/**
+     * For Search.
+     */
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.creativehub_notifications;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    final Resources res = context.getResources();
+                    return keys;
+                }
+    };
+}
